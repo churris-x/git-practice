addfc543c963bbcb10efdfe4d82c5020e43a2f4e
Francisco Altimari <francisco@altimari.nl>
2025-03-27 15:45:18 -0300

    Fix arithmetic expression

--- version.sh
+++ version.sh
@@ -9,7 +9,6 @@
 #         --tag-    semver
 # git tag v1.0.0 -m 1.0.0
 
-# Get latest semantic versioning tag
 bumpversion () {
     # Show help if missing args
     if [ $# -eq 0 ]; then 
@@ -17,7 +16,7 @@ bumpversion () {
         return 1
     fi
 
-    local TAGS VERSION
+    local TAGS VERSION UPDATE
 
     TAGS=$(git tag)
 
@@ -28,29 +27,26 @@ bumpversion () {
     # If no version found, use 0.1.0
     [ -z $VERSION ] && VERSION='v0.1.0'
 
-    echo 'version' $VERSION
-
+    # Extract major, minor and patch version numbers
     IFS=. read -r MAJOR MINOR PATCH <<< "${VERSION:1}"
 
-    echo 'MAJOR' $MAJOR
-    echo 'MINOR' $MINOR
-    echo 'PATCH' $PATCH
-
-    echo ''
-
     case $1 in
-        major ) $(($MAJOR + 1));;
-        minor ) $(($MINOR + 1));;
-        patch ) $(($PATCH + 1));;
+        major ) ((MAJOR++));;
+        minor ) ((MINOR++));;
+        patch ) ((PATCH++));;
         *) printf "Unknown argument: $1\n\nTry -h for help\n"; return 1 ;;
     esac
 
+    UPDATE="$MAJOR.$MINOR.$PATCH"
 
-    echo "v$MAJOR.$MINOR.$PATCH"
+    echo "Bumping from $VERSION => v$UPDATE"
+
+    git tag "v$UPDATE" -m $UPDATE
+
 }
 
 
-bumpversion major
+bumpversion patch
 
 
 
