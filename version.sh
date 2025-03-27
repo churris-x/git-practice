cdca19818e605ad9d83b1556da805515cdc41442
Francisco Altimari <francisco@altimari.nl>
2025-03-27 10:37:53 -0300

    Add script file for managing semver on git

---
+++ version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# MIT Francisco Altimari @1743081961
+
+# version < major | minor | patch >
+# Create a commit, create a tag with the correct message
+# Find previous versions and increment from them
+# Also allow for specific version to be added
+
+
